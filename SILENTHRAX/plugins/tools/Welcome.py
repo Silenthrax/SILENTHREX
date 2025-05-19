96fccfe1f2bc3246d5b71d43f0a35a71dfddf64d
Silenthrax <210396117+Silenthrax@users.noreply.github.com>
2025-05-19 15:28:55 +0530

    Update Welcome.py

--- SILENTHRAX/plugins/tools/Welcome.py
+++ SILENTHRAX/plugins/tools/Welcome.py
@@ -1,17 +1,12 @@
 #<<<<<<<<<<<<<<𝐒ɪʟᴇɴᴛʜʀᴀx>>>>>>>>>>>>>>#
 #<<<<<<<<<<<<<<𝐒ɪʟᴇɴᴛʜʀᴀx>>>>>>>>>>>>>>#
 
-from SILENTHRAX import app
-from pyrogram.errors import RPCError, UserAlreadyParticipant, ChatAdminRequired, InviteRequestSent, UserNotParticipant
-from pyrogram.types import ChatMemberUpdated, InlineKeyboardMarkup, InlineKeyboardButton, Message, ChatJoinRequest
-from pyrogram import Client, filters, enums
-from pyrogram.enums import ParseMode, ChatMemberStatus
-from logging import getLogger
-from SILENTHRAX.utils.database import add_served_chat, get_assistant, is_active_chat
-from SILENTHRAX.misc import SUDOERS
-import asyncio
-import random
 import os
+from PIL import ImageDraw, Image, ImageFont, ImageChops
+from pyrogram import *
+from pyrogram.types import *
+from logging import getLogger
+from SILENTHRAX import app
 
 LOGGER = getLogger(__name__)
 
@@ -23,8 +18,7 @@ class WelDatabase:
         return chat_id in self.data
 
     async def add_wlcm(self, chat_id):
-        if chat_id not in self.data:
-            self.data[chat_id] = {"state": "on"}  # Default state is "on"
+        self.data[chat_id] = {}
 
     async def rm_wlcm(self, chat_id):
         if chat_id in self.data:
@@ -40,73 +34,134 @@ class temp:
     U_NAME = None
     B_NAME = None
 
-@app.on_message(filters.command("welcome") & ~filters.private)
+def circle(pfp, size=(500, 500)):
+    pfp = pfp.resize(size, Image.LANCZOS).convert("RGBA")
+    bigsize = (pfp.size[0] * 3, pfp.size[1] * 3)
+    mask = Image.new("L", bigsize, 0)
+    draw = ImageDraw.Draw(mask)
+    draw.ellipse((0, 0) + bigsize, fill=255)
+    mask = mask.resize(pfp.size, Image.LANCZOS)
+    mask = ImageChops.darker(mask, pfp.split()[-1])
+    pfp.putalpha(mask)
+    return pfp
+
+
+def welcomepic(pic, user, chatname, id, uname):
+    background = Image.open("SILENTHRAX/assets/Kr.png")
+    pfp = Image.open(pic).convert("RGBA")
+    pfp = circle(pfp)
+    pfp = pfp.resize((1157, 1158))
+    draw = ImageDraw.Draw(background)
+    font = ImageFont.truetype('SILENTHRAX/assets/font.ttf', size=110)
+    welcome_font = ImageFont.truetype('SILENTHRAX/assets/font.ttf', size=60)
+    draw.text((1800, 700), f'NAME: {user}', fill=(255, 255, 255), font=font)
+    draw.text((1800, 830), f'ID: {id}', fill=(255, 255, 255), font=font)
+    draw.text((1800, 965), f"USERNAME : {uname}", fill=(255, 255, 255), font=font)
+    pfp_position = (391, 336)
+    background.paste(pfp, pfp_position, pfp)
+    background.save(f"downloads/welcome#{id}.png")
+    return f"downloads/welcome#{id}.png"
+
+@app.on_message(filters.command("swel") & ~filters.private)
 async def auto_state(_, message):
-    usage = "**ᴜsᴀɢᴇ:**\n**⦿ /welcome [on|off]**"
+    usage = "**ᴜsᴀɢᴇ:**\n**⦿ /swel [on|off]**"
     if len(message.command) == 1:
         return await message.reply_text(usage)
-    
     chat_id = message.chat.id
     user = await app.get_chat_member(message.chat.id, message.from_user.id)
-    
     if user.status in (
         enums.ChatMemberStatus.ADMINISTRATOR,
         enums.ChatMemberStatus.OWNER,
     ):
         A = await wlcm.find_one(chat_id)
         state = message.text.split(None, 1)[1].strip().lower()
-        
         if state == "off":
             if A:
                 await message.reply_text("**ᴡᴇʟᴄᴏᴍᴇ ɴᴏᴛɪғɪᴄᴀᴛɪᴏɴ ᴀʟʀᴇᴀᴅʏ ᴅɪsᴀʙʟᴇᴅ !**")
             else:
                 await wlcm.add_wlcm(chat_id)
                 await message.reply_text(f"**ᴅɪsᴀʙʟᴇᴅ ᴡᴇʟᴄᴏᴍᴇ ɴᴏᴛɪғɪᴄᴀᴛɪᴏɴ ɪɴ** {message.chat.title}")
-        
         elif state == "on":
             if not A:
-                await wlcm.add_wlcm(chat_id)  # Corrected from rm_wlcm to add_wlcm for "on"
-                await message.reply_text(f"**ᴇɴᴀʙʟᴇᴅ ᴡᴇʟᴄᴏᴍᴇ ɴᴏᴛɪғɪᴄᴀᴛɪᴏɴ ɪɴ** {message.chat.title}")
+                await message.reply_text("**ᴇɴᴀʙʟᴇ ᴡᴇʟᴄᴏᴍᴇ ɴᴏᴛɪғɪᴄᴀᴛɪᴏɴ.**")
+            else:
+                await wlcm.rm_wlcm(chat_id)
+                await message.reply_text(f"**ᴇɴᴀʙʟᴇᴅ ᴡᴇʟᴄᴏᴍᴇ ɴᴏᴛɪғɪᴄᴀᴛɪᴏɴ ɪɴ ** {message.chat.title}")
         else:
             await message.reply_text(usage)
     else:
         await message.reply("**sᴏʀʀʏ ᴏɴʟʏ ᴀᴅᴍɪɴs ᴄᴀɴ ᴇɴᴀʙʟᴇ ᴡᴇʟᴄᴏᴍᴇ ɴᴏᴛɪғɪᴄᴀᴛɪᴏɴ!**")
+        
 
 @app.on_chat_member_updated(filters.group, group=-3)
-async def greet_new_member(_, member: ChatMemberUpdated):
+async def greet_group(_, member: ChatMemberUpdated):
     chat_id = member.chat.id
-    count = await app.get_chat_members_count(chat_id)
     A = await wlcm.find_one(chat_id)
-
-    # If the welcome message is disabled for the chat, do nothing
-    if A:
+    if (
+        not member.new_chat_member
+        or member.new_chat_member.status in {"banned", "left", "restricted"}
+        or member.old_chat_member
+    ):
         return
-
-    user = None
-    if member.new_chat_member:
-        user = member.new_chat_member.user
-    elif member.old_chat_member:
-        user = member.old_chat_member.user
-    
-    if not user:
-        return  # Exit if no user found in the update
-    
+    user = member.new_chat_member.user if member.new_chat_member else member.from_user
     try:
-        # Welcome message
-        welcome_message = f"**👋 {user.mention}, Wᴇʟᴄᴏᴍᴇ Tᴏ {member.chat.title}!\n\n" \
-                          "• I Hᴏᴘᴇ Yᴏᴜ Aʀᴇ Fɪɴᴇ!\n\n" \
-                          "• Pʟᴇᴀsᴇ Aʟᴡᴀʏs Fᴏʟʟᴏᴡ Tʜᴇ Gʀᴏᴜᴘ Rᴜʟᴇs!\n" \
-                          "• Fʀᴇᴇ Pʀᴏᴍᴏᴛɪᴏɴ Dᴍ @Silenthrex !\n" \
-                          "────────────────────\n" \
-                          f"ᴛ ᴏ ᴛ ᴀ ʟ ᴍ ᴇ ᴍ ʙ ᴇ ʀ: {count}\n" \
-                           "────────────────────**"
-        
-        # Creating an inline button to "Join 👋" with the link
-        keyboard = InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Join 👋", url="https://t.me/silenthrex")]]
+        pic = await app.download_media(
+            user.photo.big_file_id, file_name=f"pp{user.id}.png"
         )
+    except AttributeError:
+        pic = "SILENTHRAX/assets/upic.png"
+    if (temp.MELCOW).get(f"welcome-{member.chat.id}") is not None:
+        try:
+            await temp.MELCOW[f"welcome-{member.chat.id}"].delete()
+        except Exception as e:
+            LOGGER.error(e)
+    try:
+        welcomeimg = welcomepic(
+            pic, user.first_name, member.chat.title, user.id, user.username
+        )
+        temp.MELCOW[f"welcome-{member.chat.id}"] = await app.send_photo(
+            member.chat.id,
+            photo=welcomeimg,
+            caption=f"""
 
-        # Send the welcome message with the inline button
-        await app.send_message(chat_id, welcome_message, reply_markup=keyboard)
+❣️𝐖ᴇʟᴄᴏᴍᴇ 𝐈ɴ 𝐍ᴇᴡ 𝐆ʀᴏᴜᴘ ❣️
+➖➖➖➖➖➖➖➖➖➖➖➖
+🏘{member.chat.title}🥳
+➖➖➖➖➖➖➖➖➖➖➖➖
+● 𝐍ᴀᴍᴇ ➥ {user.mention} 
+● 𝐔ꜱᴇʀɴᴀᴍᴇ ➥ @{user.username} 
+
+┏━━━━━━━━━━━━━━━
+┣ 𝟏 ➥ ᴅᴏɴᴛ ᴀʙᴜsɪɴɢ  👣
+
+┣ 𝟐 ➥ 💗𝐑ᴇsᴘᴇ𝐂ᴛ 𝐄ᴠᴇʀ𝐘 𝐁ᴏ𝐘 𝐅ᴇᴇʟɪɴ𝐆
+
+┣ 𝟑 ➥ ʟɪɴᴋ ɴᴏᴛ ᴀʟʟᴏᴡ 
+
+┣ 𝟒 ➥ ᴅᴏɴᴛ sᴇɴᴅ ᴀᴅᴜʟᴛ sᴛᴜғғ
+┗━━━━━━━━━━━━━━━━━      
+
+❖𝐌ᴀᴅᴇ  𝐁ʏ [𝐒ɪʟᴇɴᴛʜʀᴀx](https://t.me/SILENTHEX)
+""",
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"𝐀ᴅᴅ 𝐌ᴇ 𝐁ᴀʙʏ", url=f"https://t.me/LISA_UFC_BOT?startgroup=true")]])
+        )
     except Exception as e:
-        LOGGER.error(f"Error sending welcome message: {e}")
+        LOGGER.error(e)
+    try:
+        os.remove(f"downloads/welcome#{user.id}.png")
+        os.remove(f"downloads/pp{user.id}.png")
+    except Exception as e:
+        pass
+
+@app.on_message(filters.new_chat_members & filters.group, group=-1)
+async def bot_wel(_, message):
+    for u in message.new_chat_members:
+        if u.id == app.me.id:
+            await app.send_message(LOG_CHANNEL_ID, f"""
+NEW GROUP
+➖➖➖➖➖➖➖➖➖➖➖➖
+NAME: {message.chat.title}
+ID: {message.chat.id}
+USERNAME: @{message.chat.username}
+➖➖➖➖➖➖➖➖➖➖➖➖
+""") 
